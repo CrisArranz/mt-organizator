b9e85d6cd363dc20ed0efc56a1a7829f1ba4ba73
EstherCM <esther.carreton8@gmail.com>
2023-04-01 14:23:27 +0200

    API: Updates routes with middlewares

--- api/config/routes.config.js
+++ api/config/routes.config.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const { user, tournament, auth } = require("../controllers");
-const { secure } = require("../middlewares");
+const { secure, tournamentSecure, userSecure } = require("../middlewares");
 
 router.post("/register", auth.register);
 router.post("/login", auth.login);
@@ -10,11 +10,12 @@ router.delete("/logout", secure.isLogged, auth.logout);
 
 
 router.get("/users", secure.isLogged, secure.isAdmin, user.getUser);
-router.get("/users/:nickname", secure.isLogged, secure.isYourAccount, user.getUser);
-router.patch("/users/:nickname", secure.isLogged, secure.isYourAccount, user.getUser);
+router.get("/users/:nickname", secure.isLogged, secure.isYourAccountOrAdmin, userSecure.getUserCriterial, user.getUser);
+router.patch("/users/:nickname", secure.isLogged, secure.isYourAccountOrAdmin, userSecure.getUserCriterial, userSecure.existsUnique, user.updateUser);
 
 router.get("/tournaments", secure.isLogged, secure.isAdmin, tournament.getTournament);
 router.get("/tournaments/:idTournament", secure.isAdmin, tournament.getTournament);
+router.delete("/tournaments/:idTournament", secure.isAdmin, tournamentSecure.existsTournament, tournament.deleteTournament);
 router.post("/tournaments", secure.isLogged, secure.isAdmin, tournament.create);
 
 module.exports = router;
